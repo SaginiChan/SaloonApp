82b42f2820519253f25bc0b57bbc704887e8734f
PeteHack <pwachira900@gmail.com>
2021-05-23 00:40:11 +0300

    fix navigation to home fragment

--- app/src/main/java/com/example/myapplication/Auth/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/Auth/LoginFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.Pages.Home
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentLoginBinding
@@ -13,19 +14,25 @@ import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
 class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? =null
-    private val  binding get() = _binding!!
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding= FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         binding.tohomepagebutton.setOnClickListener {
-            val intent =Intent (activity, Home::class.java)
-            activity?.startActivity(intent)
+
+            /*
+            since you are using nav graph instead of using intents to switch between activities,
+            you can invoke the actions on the navgraph to navigate to the preferred destination
+             to understand better check https://medium.com/swlh/android-navigation-component-part-1-6191323eaf39
+             https://developer.android.com/guide/navigation/navigation-getting-started*/
+
+            NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_home2)
 
         }
 
@@ -35,7 +42,7 @@ class LoginFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding =null
+        _binding = null
     }
 
 }
